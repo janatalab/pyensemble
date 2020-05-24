8ef0ee084ef87ec63e9dd86d16f8afafad26e747
Angela Nazarian <anazarian@ucdavis.edu>
2020-05-23 22:39:05 -0700

    Update jingles.py

--- pyensemble/experiments/jingles/jingles.py
+++ pyensemble/experiments/jingles/jingles.py
@@ -165,13 +165,13 @@ def age_meets_criterion_and_lived_in_USA(request,*args,**kwargs):
     form_name='jingle_project_demographics'
     
     # Get the response corresponding to Question 2 from this form
-    #USA_response = Response.objects.filter(session=kwargs['session_id'],form__name=form_name, question__text__contains='born').last()
-    USA_response = Response.objects.filter(session=kwargs['session_id'],form__name=form_name, question__text__contains='born').choices()
+    USA_response = Response.objects.filter(session=kwargs['session_id'],form__name=form_name, question__text__contains='born').last()
 
     # Display this form if they moved to the USA at 6 years of age or older
-    return ("6" in USA_response.value or "10" in USA_response.value)
+    return USA_response.response_enum > 0
+    #return ("6" in USA_response.value or "10" in USA_response.value)
     #return ("6" in USA_response.enum_values or "10" in USA_response.enum_values)
-    #return USA_response.response_enum > 4
+    
 
 def age_meets_criterion(request,*args,**kwargs):
     #
