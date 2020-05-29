2b9478eeb5f8984bae1bfcb18897eb5d73835b9d
peachypunk <anazarian@ucdavis.edu>
2020-05-29 04:59:14 +0000

    updated f-strings in print statements

--- pyensemble/experiments/jingles/jingles.py
+++ pyensemble/experiments/jingles/jingles.py
@@ -64,13 +64,13 @@ def import_stims(request,stimdir=stimdir):
         # Get an attribute corresponding to this media type
         attribute, _ = Attribute.objects.get_or_create(name='Media Type', attribute_class='stimulus')
 
-        print(f'Working on {media_type}s ...')
+        print("Working on", media_type)
 
         # Loop over files in the directory
         with os.scandir(os.path.join(stimdir,media_type)) as stimlist:
             for stim in stimlist:
                 if not stim.name.startswith('.') and stim.is_file():
-                    print(f'\tImporting {stim.name}')
+                    print("Importing", stim.name)
 
                     # Strip off the extension
                     fstub,fext = os.path.splitext(stim.name)
@@ -126,7 +126,7 @@ def import_attributes(request):
                 try:
                     stimulus = Stimulus.objects.get(name=stimname)
                 except:
-                    print(f'Unable to locate stimulus with name {stimname}')
+                    print("Unable to locate stimulus with name", stimname)
                     continue
 
                 # Iterate over our attributes
@@ -156,9 +156,67 @@ def import_attributes(request):
     return render(request, template, context)
 
 @login_required
+def update_attributes(request):
+    if request.method == 'POST':
+        form = ImportForm(request.POST, request.FILES)
+                         
+        if form.is_valid():
+            csv_file = request.files['file']
+            csv_file = TextIOWrapper(csv_file, encoding='utf-8')
+            reader = csv.reader(csv_file, delimiter=',')
+        
+            # Get the column headers
+            columns = next(reader)
+
+            # Get a dictionary of column indexes
+            cid = {col:idx for idx, col in enumerate(columns)}
+
+            # Specify our column to attribute mapping
+            colattmap = {
+                'Company': 'Company',
+                'Item': 'Product',
+                'Product_Category': 'Product Category',
+                'First_Played': 'First Played',
+                'Last_Played': 'Last Played',
+                'Region_Played': 'Region',
+                'Modality': 'Modality',
+            }
+            numeric = ['First_Played','Last_Played']
+
+            # Iterate over the list of stims to update
+            #ideally, this should be more flexible rather than dicatating stim id's here
+            stims_to_change = ['151_Miller_logo2', '36_DrPepper_tagline9', '52_KellogsFrostedFlakes_tagline6', '631_MrPotatoHead_logo2', '797_Tide_tagline5', '90_Beachnutgum_logo7', '90_Beachnutgum_tagline7', '98_Kool-Aid_comm3', '98_Kool-Aid_tagline4', 'Converse_tagline', 'GirlTalk_comm', 'GreenGiant_comm3', 'Hormel_comm', 'Hormel_tagline5', 'Hormel_tagline6', 'Jif_tagline_02', 'Oxydol_comm']
+
+            for stim in stims_to_change:
+                # Find the row in our csv containing values for this stimulus
+                row = csv_file[csv_file.Stimulus_ID == stim]
+
+                # Iterate over our attributes
+                for col in colattmap:
+
+                    # Get the attribute value we're going to write
+                    value = row[col].values[0]
+                    if col in numeric:
+                        value_text = ''
+                        value_float = float(value) if value and value != '?' else None
+                    else:
+                        value_text = value
+                        value_float = None
+
+                    # Replace this attribute in the stim x attribute table
+                    sxa, created = StimulusXAttribute.objects.get_or_create(stimulus=stimulus, attribute=attribute, attribute_value_text=value_text, attribute_value_double=value_float)
+
+                    #you can only use this return statement if you're defining a function
+                    return render(request,'pyensemble/message.html',{'msg':'Successfully imported the attribute file'})
+                    pdb.set_trace()
+
+
+@login_required
 def delete_exf(request,title):
     ExperimentXForm.objects.filter(experiment__title=title).delete()
-    return render(request,'pyensemble/message.html',{'msg':f'Deleted experimentxform for {title}'})
+   # return render(request,'pyensemble/message.html',{'msg':'Deleted experimentxform for', title})
+
+    return render(request,'pyensemble/message.html',{'msg':'Deleted experimentxform'})
 
 def age_meets_criterion_and_lived_in_USA(request,*args,**kwargs):
     #
@@ -435,7 +493,7 @@ def select_study1(request,*args,**kwargs):
         if settings.DEBUG:
             print('Created a slogan trial: %s'%(trial['stimulus']))
     else:
-        raise ValueError(f'Cannot specify trial for {media_type}')
+        raise ValueError('Cannot specify trial')
 
     # Push the trial to the timeline
     timeline.append(trial)
