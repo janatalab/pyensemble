26748a92077ef9ddfb9f8ea61d32431f6b742f33
bmk22 <benkubit@gmail.com>
2021-04-04 16:53:07 +0000

    hide button mods

--- pyensemble/static/pyensemble/js/trialrunner.js
+++ pyensemble/static/pyensemble/js/trialrunner.js
@@ -13,6 +13,8 @@ function post_trial(data){
     // Unmask the div containing the questions
     $("#questions").removeClass("d-none");
 
+    $("#questions .form-actions input").attr({'disabled':false})
+
 }
 
 function start_trial(timeline){
@@ -21,6 +23,8 @@ function start_trial(timeline){
     options.timeline = timeline;
     options.show_progress_bar = false;
     options.on_finish = post_trial;
+
+    $("#questions .form-actions input").attr({'disabled':true})
 
     // Initialize jsPsych
     jsPsych.init(options);
