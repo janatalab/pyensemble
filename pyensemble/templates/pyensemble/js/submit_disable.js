c0f24f5c85ba4a18708ec5c88cc5ea3362b19446
Janata Lab <janatalab@gmail.com>
2022-09-18 21:29:43 -0700

    removed non-implemented submit button disabling (#150)
    Co-authored-by: Petr Janata <pjanata@ucdavis.edu>

--- pyensemble/templates/pyensemble/js/submit_disable.js
+++
@@ -1,7 +0,0 @@
-    $('#question-form').submit(function(e){
-        if ($("input[name='submit']").attr("attempted") == true){
-            e.preventDefault();
-        } else {
-            $("input[name='submit']").attr("attempted",'true');
-        }
-    });
