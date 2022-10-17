ae24d99ed6d5e7e8d68bbfd9d1a3ef5487a4ff5b
Janata Lab <janatalab@gmail.com>
2022-10-17 16:26:56 -0700

    adding debug experiment functionality (#154)
    Co-authored-by: Petr Janata <pjanata@ucdavis.edu>

---
+++ pyensemble/experiments/debug/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+#
+# Endpoints for the default debug experiment
+
+from django.urls import path
+
+from . import group
+
+app_name = 'debug'
+
+urlpatterns = [
+    path('group/create_experiment', group.create_experiment),
+]
