52424cae0257ca6dd65742d1ece72cf1755bf4c8
Petr Janata <pjanata@ucdavis.edu>
2023-02-05 15:32:37 -0800

    caching notification progress

---
+++ pyensemble/experiments/debug/conditions.py
@@ -0,0 +1,5 @@
+# conditions.py
+
+def desire_further_participation(request, *args, **kwargs):
+    
+    return True
--- pyensemble/experiments/debug/urls.py
+++ pyensemble/experiments/debug/urls.py
@@ -4,10 +4,12 @@
 
 from django.urls import path
 
-from . import group
+from . import views, tasks, group
 
 app_name = 'debug'
 
 urlpatterns = [
-    path('group/create_experiment/', group.create_experiment),
+    path('', views.home, name='home'),
+    path('create/experiment/', tasks.create_experiment, name='create-experiment'),
+    path('group/create_experiment/', group.create_group_experiment, name='create-group-experiment'),
 ]
