fa30fcdc2bdf3782f1dcc2ad557f484f262fd21c
Petr Janata <pjanata@ucdavis.edu>
2023-03-03 07:35:42 +0000

    ticket validity checking

--- pyensemble/views.py
+++ pyensemble/views.py
@@ -380,6 +380,10 @@ def run_experiment(request, experiment_id=None):
         if ticket.type == 'user' and ticket.used:
             return errors.ticket_error(request, ticket, 'TICKET_ALREADY_USED')
 
+        # Make sure the ticket's validity has commenced
+        if timezone.now() < ticket.start:
+            return errors.ticket_error(request, ticket, 'TICKET_NOT_YET_VALID')
+
         # Make sure ticket hasn't expired
         if ticket.expired:
             return errors.ticket_error(request, ticket, 'TICKET_EXPIRED')
