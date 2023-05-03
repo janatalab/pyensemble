05a7516d0c31ceb0b143f0f2f2174c045995c210
Petr Janata <pjanata@ucdavis.edu>
2023-05-03 07:15:52 +0000

    removed nonfunctioning urls

--- pyensemble/integrations/urls.py
+++ pyensemble/integrations/urls.py
@@ -1,15 +1,12 @@
 # urls.py
 
 from django.urls import path
-from .spotify import SpotifyBaseView, spotify_login, spotify_profile, spotify_callback, spotify_search, spotify_import
+from .spotify import SpotifyBaseView, spotify_search, spotify_import
 
 app_name = 'integrations'
 
 urlpatterns = [
     path('spotify/', SpotifyBaseView.as_view(), name='spotify_base'),
-    path('spotify/login/', spotify_login, name='spotify_login'),
-    path('spotify/callback/', spotify_callback, name='spotify_callback'),
-    path('spotify/profile/', spotify_profile, name='spotify_profile'),
     path('spotify/search/', spotify_search, name='spotify-search'),
     path('spotify/import/', spotify_import, name='spotify-import'),    
 ]
