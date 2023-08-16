eb3d1d852781c1adc6cf93ad1fb98acc6063f108
Petr Janata <pjanata@ucdavis.edu>
2023-08-16 00:24:12 +0000

    improved stimulus importer

--- pyensemble/importers/tasks.py
+++ pyensemble/importers/tasks.py
@@ -5,7 +5,7 @@ import csv, json
 
 from django.conf import settings
 
-from pyensemble.models import Stimulus
+from pyensemble.models import Stimulus, Attribute, StimulusXAttribute
 
 import pdb
 
@@ -13,26 +13,29 @@ def clean_string(item):
     return item if item else ''
 
 def clean_boolean(item):
-    if item=='T':
+    if item == 'T':
         value = True
-    elif item=='F':
-        value=False
+    elif item == 'F':
+        value = False
     else:
         value=None
     return value
 
-def process_stimulus_table(file):
-    #
-    # This method hasn't been fully implemented yet
-    #
-    result = {}
+def process_stimulus_table(data):
+    result = {
+        'num_submitted': 0,
+        'num_existing': 0,
+        'num_written': 0,
+        'failed': [],
+    }
+
+    file = data['file']
 
     # Get our filename and extension
     fstub, fext = os.path.splitext(file.name)
 
     # Wrap our input stream
     txt_file = io.TextIOWrapper(file.file)
-
 
     if fext == ".json":
         # Load in the json data
@@ -57,15 +60,36 @@ def process_stimulus_table(file):
 
         # Rewind the file
         txt_file.seek(0)
+
+        # Get our file reader
         reader = csv.reader(txt_file, dialect)
 
         # Get the column headers
         columns = next(reader)
 
-        # Make sure the column headers in the import file match Stimulus table fields (with the exception of the file_source column which specifies the path to the local file)
+        # NOTE: Microsoft Excel might preprend junk to the first column name when saving out to a .csv file, which will prevent the information from the column being written to the stimulus table. If exporting from Excel to a .csv file, open the .csv file in a basic text editor and save it out again to get rid of the hidden characters.
+
+        # Verify that we have location and name columns
+        if 'name' not in columns:
+            raise ValueError("The .csv file must have a column named 'name'.")
+
+        if 'location' not in columns:
+            raise ValueError("The .csv file must have a column named 'location' which contains the name of the actual stimulus file.")
 
         # Get a dictionary of column indexes
         cid = {col:idx for idx, col in enumerate(columns)}
+
+        # Determine which of the columns correspond to fields in the Stimulus table
+        fields = [f.name for f in Stimulus._meta.fields]
+
+        # Get columns that are also fields in the Stimulus table
+        valid_columns = list(set(columns) & set(fields))
+
+        # Get any attribute columns
+        attribute_cols = []
+        for col in columns:
+            if col.startswith('attribute'):
+                attribute_cols.append(col)
 
         # Iterate over the rows
         nstim=0
@@ -75,13 +99,57 @@ def process_stimulus_table(file):
 
             print(f"Processing stimulus {nstim}: {name}")
 
+            # Create the data that we're going to write to the db
+            entry = {}
+            for cname in valid_columns:
+                entry.update({cname: row[cid[cname]]})
+
+            # Prepend our location_root to our location information
+            entry['location'] = os.path.join(data['location_root'], entry['location'])
+
             # Get or create the Stimulus object
-            stimulus, created = Stimulus.objects.get_or_create(
-                name=row[cid['name']],
-                )
+            stimulus, created = Stimulus.objects.get_or_create(**entry)
+
+            if created:
+                result['num_written'] += 1
+            else:
+                result['num_existing'] += 1
 
             # Deal with copying the file from local storage to destination storage
-            # We probably want to update our Stimulus table so that we have not only the destination path, but also a FileField that can automatically handle URL creation 
+
+            # Link any specified attribute info
+            for attribute in attribute_cols:
+                attrib_parts = attribute.split('__')
+
+                # Get our attribute nmae
+                attrib_name = attrib_parts[1]
+
+                # Get the attribute entry
+                attrib, _ = Attribute.objects.get_or_create(name=attrib_name)
+
+                # Get the attribute data value type
+                try:
+                    attrib_value_type = attrib_parts[2]
+                except:
+                    attrib_value_type = 'text'
+
+                # Create or get our stimulus x attribute object
+                sxa, _ = StimulusXAttribute.objects.get_or_create(
+                    stimulus=stimulus, 
+                    attribute=attrib, 
+                )
+
+                # Update the value
+                attribute_value = row[cid[attribute]]
+                if attrib_value_type == 'text':
+                    sxa.attribute_value_text = attribute_value
+                elif attrib_value_type == 'double':
+                    sxa.attribute_value_double = attribute_value
+
+                # Save the sxa entry with the value
+                sxa.save()
+
+        result['num_submitted'] = nstim
 
     return result
 
