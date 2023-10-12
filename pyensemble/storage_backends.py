4f06d42a869327d80b0497405c7eb6febcd1348d
Petr Janata <pjanata@ucdavis.edu>
2023-10-12 23:48:07 +0000

    implementing S3 storage

---
+++ pyensemble/storage_backends.py
@@ -0,0 +1,14 @@
+# storage_backends.py
+
+from django.conf import settings
+
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class S3MediaStorage(S3Boto3Storage):
+    bucket_name = settings.AWS_MEDIA_STORAGE_BUCKET_NAME
+    file_overwrite = False
+
+
+class S3DataStorage(S3Boto3Storage):
+    bucket_name = settings.AWS_DATA_STORAGE_BUCKET_NAME
+    file_overwrite = False
